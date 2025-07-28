18ae4a6b916d4504fb91a64efa8c28b5ee3f32c6
juanmatorress10 <juanmatorress10@gmail.com>
2025-07-28 17:15:20 +0200

    Segundo intento modificacino de rutas

--- xmoker-app/src/app/services/auth.service.ts
+++ xmoker-app/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment'; // ✅ Importamos environment
+import { environment } from 'src/environments/environment'; 
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
